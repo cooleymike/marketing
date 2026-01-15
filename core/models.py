29ef66f7429eb0ec3e3216597bc7599515c77f6f
Mike Cooley <cooley.mike1@gmail.com>
2026-01-15 11:51:37 -0300

    fix: added to TODO

--- core/models.py
+++ core/models.py
@@ -127,6 +127,7 @@ class Expense(models.Model):
     #     extract month from the current created date
         print(self.created_date.month)
 
+    # TODO: To complete by the weekend
     # if month:
     #     jan, feb, march
     #     return q1
@@ -142,9 +143,6 @@ class Expense(models.Model):
     #     return q4
     #
     # return self.created_date.month
-#
-    #
-
 
     @property
     def remaining_budget(self):
