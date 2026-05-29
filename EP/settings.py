038a7586b4c83d5b8424be48b57af73c3080ff57
Mike Cooley <cooley.mike1@gmail.com>
2026-05-29 11:09:23 -0300

    fix: fixed logic and messaging on manager approval page added email response (#56)

--- EP/settings.py
+++ EP/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emails',
     'core',
     'accounts',
     'django.contrib.sites',
